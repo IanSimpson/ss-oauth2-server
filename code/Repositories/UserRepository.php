9f24a5537502090fe850198094c7bc8a35064548
Mateusz Uzdowski <mateusz@silverstripe.com>
2018-10-04 13:41:41 +1300

    Implement missing interface methods.

--- code/Repositories/UserRepository.php
+++ code/Repositories/UserRepository.php
@@ -6,11 +6,19 @@
 
 namespace IanSimpson\Repositories;
 
+use Exception;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Repositories\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface
 {
-    /**
-     * {@inheritdoc}
-     */
+    public function getUserEntityByUserCredentials(
+        $username,
+        $password,
+        $grantType,
+        ClientEntityInterface $clientEntity
+    )
+    {
+        throw new Exception('Unsupported yet');
+    }
 }
